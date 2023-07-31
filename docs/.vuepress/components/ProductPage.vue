d41430a29a3ba13dff1c285357eeec5c0a21b8e4
switchII <luoandon@gmail.com>
2023-07-31 23:18:13 +0800

    feat:优化产品体系和解决方案

--- docs/.vuepress/components/ProductPage.vue
+++ docs/.vuepress/components/ProductPage.vue
@@ -7,14 +7,14 @@
         </div> -->
 
         <div class="crs-page-panel">
-            <div class="crs-page-items-panel" v-for="item in 7" :key="item">
-                <div class="crs-page-box">应用现代化改造</div>
-                <div class="crs-page-box-desc">评估、规划、实施和衡量软件实践与功能，从而对贵组织的业务应用组合进行现代化改造和简化。</div>
+            <div class="crs-page-items-panel" v-for="item in productList" :key="item.id">
+                <div class="crs-page-box">{{ item.model }}</div>
+                <div class="crs-page-box-desc">{{ item.desc }}</div>
                 <div class="item-list-box">
                     <ul>
-                        <li class="crs-page-item" v-for="i in 8" :key="i">
-                            <div class="item-title">FinOps 和 GKE 优化</div>
-                            <div class="item-desc">在 GKE 上运行可靠、高性能且经济实惠的应用的最佳实践。</div>
+                        <li class="crs-page-item" v-for="i in item.services" :key="i.id">
+                            <div class="item-title">{{ i.name }}</div>
+                            <div class="item-desc">{{ i.desc }} 、高性能且经济实惠的应用的最佳实践。</div>
                         </li>
                     </ul>
                 </div>
@@ -23,6 +23,399 @@
 
     </div>
 </template>
+<script>
+
+export default {
+    data() {
+        return {
+            productList: [
+                {
+                    "id": 1,
+                    "model": "核心框架",
+                    "desc": "核心框架模块包括核心开发框架、前端开发框架（桌面）和前端开发框架（移动）。",
+                    "services": [
+                        {
+                            "id": 1,
+                            "name": "01. 核心开发框架",
+                            "desc": "用于核心开发的框架。",
+                            "code": "alinesno-infra-common"
+                        },
+                        {
+                            "id": 2,
+                            "name": "02. 前端开发框架（桌面）",
+                            "desc": "用于桌面前端开发的框架。",
+                            "code": "alinesno-infra-ui"
+                        },
+                        {
+                            "id": 3,
+                            "name": "03. 前端开发框架（移动）",
+                            "desc": "用于移动前端开发的框架。",
+                            "code": "alinesno-infra-app"
+                        }
+                    ]
+                },
+                {
+                    "id": 5,
+                    "model": "基础服务",
+                    "desc": "基础服务模块包括权限配置服务、代码生成器、分布式配置服务、事务消息服务、通知管理服务、存储管理服务、单点登录服务、网关配置服务、安全验证码服务、IM消息服务、邮箱管理服务、文档搜索服务、分布式ID服务、敏感词过滤服务、支付服务、会员服务、内容服务、商城服务、流程中心服务和文档查看服务。",
+                    "services": [
+                        {
+                            "id": 5,
+                            "name": "01. 权限配置服务",
+                            "desc": "用于配置权限的服务。",
+                            "code": "alinesno-infra-base-authority"
+                        },
+                        {
+                            "id": 6,
+                            "name": "02. 代码生成器",
+                            "desc": "用于生成代码的工具。",
+                            "code": "alinesno-infra-base-init"
+                        },
+                        {
+                            "id": 7,
+                            "name": "03. 分布式配置服务",
+                            "desc": "用于分布式配置的服务。",
+                            "code": "alinesno-infra-base-config"
+                        },
+                        {
+                            "id": 8,
+                            "name": "04. 事务消息服务",
+                            "desc": "用于处理事务消息的服务。",
+                            "code": "alinesno-infra-base-message"
+                        },
+                        {
+                            "id": 9,
+                            "name": "05. 通知管理服务",
+                            "desc": "用于管理通知的服务。",
+                            "code": "alinesno-infra-base-notices"
+                        },
+                        {
+                            "id": 10,
+                            "name": "06. 存储管理服务",
+                            "desc": "用于管理存储的服务。",
+                            "code": "alinesno-infra-base-storage"
+                        },
+                        {
+                            "id": 11,
+                            "name": "07. 单点登陆服务",
+                            "desc": "用于实现单点登录的服务。",
+                            "code": "alinesno-infra-base-sso"
+                        },
+                        {
+                            "id": 12,
+                            "name": "08. 网关配置服务",
+                            "desc": "用于配置网关的服务。",
+                            "code": "alinesno-infra-base-gateway"
+                        },
+                        {
+                            "id": 13,
+                            "name": "09. 安全验证码服务",
+                            "desc": "用于生成安全验证码的服务。",
+                            "code": "alinesno-infra-base-validate"
+                        },
+                        {
+                            "id": 14,
+                            "name": "10. IM消息服务",
+                            "desc": "用于处理即时消息的服务。",
+                            "code": "alinesno-infra-base-im"
+                        },
+                        {
+                            "id": 15,
+                            "name": "11. 邮箱管理服务",
+                            "desc": "用于管理邮箱的服务。",
+                            "code": "alinesno-infra-base-email"
+                        },
+                        {
+                            "id": 16,
+                            "name": "12. 文档搜索服务",
+                            "desc": "用于搜索文档的服务。",
+                            "code": "alinesno-infra-base-document"
+                        },
+                        {
+                            "id": 17,
+                            "name": "13. 分布式ID服务",
+                            "desc": "用于生成分布式ID的服务。",
+                            "code": "alinesno-infra-base-id"
+                        },
+                        {
+                            "id": 18,
+                            "name": "14. 敏感词过滤服务",
+                            "desc": "用于过滤敏感词的服务。",
+                            "code": "alinesno-infra-base-sensitive"
+                        },
+                        {
+                            "id": 19,
+                            "name": "15. 支付服务",
+                            "desc": "用于处理支付的服务。",
+                            "code": "alinesno-infra-base-pay"
+                        },
+                        {
+                            "id": 20,
+                            "name": "16. 会员服务",
+                            "desc": "用于管理会员的服务。",
+                            "code": "alinesno-infra-base-member"
+                        },
+                        {
+                            "id": 21,
+                            "name": "17. 内容服务",
+                            "desc": "用于管理内容的服务。",
+                            "code": "alinesno-infra-base-cms"
+                        },
+                        {
+                            "id": 22,
+                            "name": "18. 商城服务",
+                            "desc": "用于管理商城的服务。",
+                            "code": "alinesno-infra-base-shop"
+                        },
+                        {
+                            "id": 23,
+                            "name": "19. 流程中心服务",
+                            "desc": "用于流程中心的服务。",
+                            "code": "alinesno-infra-base-workflow"
+                        },
+                        {
+                            "id": 24,
+                            "name": "20. 文档查看服务",
+                            "desc": "用于查看文档的服务。",
+                            "code": "alinesno-infra-base-fileshow"
+                        },
+                        {
+                            "id": 25,
+                            "name": "21. 基设平台管理服务",
+                            "desc": "用于管理基设平台的服务，定位于管理员。",
+                            "code": "alinesno-infra-base-platform"
+                        }
+                    ]
+                },
+                {
+                    "id": 27,
+                    "model": "数据服务",
+                    "desc": "数据服务模块包括主数据服务、数据上报服务、数据集成服务、数据开发服务、实时计算服务、数据安全服务、数据质量服务、数据总线服务、数据资产服务和数据算法服务。",
+                    "services": [
+                        {
+                            "id": 27,
+                            "name": "01. 主数据服务",
+                            "desc": "用于主数据管理的服务。",
+                            "code": "alinesno-infra-data-mdm"
+                        },
+                        {
+                            "id": 28,
+                            "name": "02. 数据上报服务",
+                            "desc": "用于数据上报的服务。",
+                            "code": "alinesno-infra-data-report"
+                        },
+                        {
+                            "id": 29,
+                            "name": "03. 数据集成服务",
+                            "desc": "用于数据集成的服务。",
+                            "code": "alinesno-infra-data-kettle"
+                        },
+                        {
+                            "id": 30,
+                            "name": "04. 数据开发服务",
+                            "desc": "用于数据开发的服务。",
+                            "code": "alinesno-infra-data-dolphinscheduler"
+                        },
+                        {
+                            "id": 31,
+                            "name": "05. 实时计算服务",
+                            "desc": "用于实时计算的服务。",
+                            "code": "alinesno-infra-data-flink"
+                        },
+                        {
+                            "id": 32,
+                            "name": "06. 数据安全服务",
+                            "desc": "用于数据安全的服务。",
+                            "code": "alinesno-infra-data-security"
+                        },
+                        {
+                            "id": 33,
+                            "name": "07. 数据质量服务",
+                            "desc": "用于数据质量的服务。",
+                            "code": "alinesno-infra-data-quarity"
+                        },
+                        {
+                            "id": 34,
+                            "name": "08. 数据总线服务",
+                            "desc": "用于数据总线的服务。",
+                            "code": "alinesno-infra-data-bus"
+                        },
+                        {
+                            "id": 35,
+                            "name": "09. 数据资产服务",
+                            "desc": "用于数据资产管理的服务。",
+                            "code": "alinesno-infra-data-assets"
+                        },
+                        {
+                            "id": 36,
+                            "name": "10. 数据算法服务",
+                            "desc": "用于数据算法的服务。",
+                            "code": "alinesno-infra-data-algorithm"
+                        }
+                    ]
+                },
+                {
+                    "id": 38,
+                    "model": "智能服务",
+                    "desc": "智能服务模块包括智能推荐服务、智能搜索服务、智能语音服务、智能图像服务、智能视频服务和智能机器人服务。",
+                    "services": [
+                        {
+                            "id": 38,
+                            "name": "01. 智能推荐服务",
+                            "desc": "用于智能推荐的服务。",
+                            "code": "alinesno-infra-ai-recommendation"
+                        },
+                        {
+                            "id": 39,
+                            "name": "02. 智能搜索服务",
+                            "desc": "用于智能搜索的服务。",
+                            "code": "alinesno-infra-ai-search"
+                        },
+                        {
+                            "id": 40,
+                            "name": "03. 智能语音服务",
+                            "desc": "用于智能语音的服务。",
+                            "code": "alinesno-infra-ai-speech"
+                        },
+                        {
+                            "id": 41,
+                            "name": "04. 智能图像服务",
+                            "desc": "用于智能图像的服务。",
+                            "code": "alinesno-infra-ai-image"
+                        },
+                        {
+                            "id": 42,
+                            "name": "05. 智能视频服务",
+                            "desc": "用于智能视频的服务。",
+                            "code": "alinesno-infra-ai-video"
+                        },
+                        {
+                            "id": 43,
+                            "name": "06. 智能机器人服务",
+                            "desc": "用于智能机器人的服务。",
+                            "code": "alinesno-infra-ai-robot"
+                        }
+                    ]
+                },
+                {
+                    "id": 43,
+                    "model": "运维服务",
+                    "desc": "运维服务模块包括自动化任务服务（业务自动运维服务）、分布式日志服务、分布式链路跟踪服务、持续集成服务、容器管理服务和监控预警服务。",
+                    "services": [
+                        {
+                            "id": 1,
+                            "name": "自动化任务服务（业务自动运维服务）",
+                            "desc": "用于自动化运维任务的服务。",
+                            "code": "alinesno-infra-ops-scheduler"
+                        },
+                        {
+                            "id": 2,
+                            "name": "分布式日志服务",
+                            "desc": "用于分布式日志管理的服务。",
+                            "code": "alinesno-infra-ops-logger"
+                        },
+                        {
+                            "id": 3,
+                            "name": "分布式链路跟踪服务",
+                            "desc": "用于分布式链路跟踪的服务。",
+                            "code": "alinesno-infra-ops-telemetry"
+                        },
+                        {
+                            "id": 4,
+                            "name": "持续集成服务",
+                            "desc": "用于持续集成的服务。",
+                            "code": "alinesno-infra-ops-cicd"
+                        },
+                        {
+                            "id": 5,
+                            "name": "容器管理服务",
+                            "desc": "用于容器管理的服务。",
+                            "code": "alinesno-infra-ops-container"
+                        },
+                        {
+                            "id": 6,
+                            "name": "监控预警服务",
+                            "desc": "用于监控和预警的服务。",
+                            "code": "alinesno-infra-ops-watcher"
+                        }
+                    ]
+                },
+                {
+                    "id": 57,
+                    "model": "业务服务",
+                    "desc": "业务服务模块包括商品秒杀服务、实时推荐服务、实时画像服务和无代码开发服务。",
+                    "services": [
+                        {
+                            "id": 1,
+                            "name": "商品秒杀服务",
+                            "desc": "用于商品秒杀的服务。",
+                            "code": "alinesno-infra-bus-limit"
+                        },
+                        {
+                            "id": 2,
+                            "name": "实时推荐服务",
+                            "desc": "用于实时推荐的服务。",
+                            "code": "alinesno-infra-bus-recommend"
+                        },
+                        {
+                            "id": 3,
+                            "name": "实时画像服务",
+                            "desc": "用于实时画像服务",
+                            "code": "alinesno-infra-bus-profiling"
+                        },
+                        {
+                            "id": 4,
+                            "name": "无代码开发服务",
+                            "desc": "用于无代码开发服务",
+                            "code": "alinesno-infra-bus-nocode"
+                        }
+                    ]
+                },
+                {
+                    "id": 50,
+                    "model": "运营服务",
+                    "desc": "运维服务模块包括自动化任务服务（业务自动运维服务）、分布式日志服务、分布式链路跟踪服务、持续集成服务、容器管理服务和监控预警服务。",
+                    "services": [
+                        {
+                            "id": 1,
+                            "name": "基设平台服务",
+                            "desc": "添加架构排版服务",
+                            "code": "alinesno-infra-plat-saas"
+                        },
+                        {
+                            "id": 2,
+                            "name": "安全感触服务",
+                            "desc": "用于安全感触服务",
+                            "code": "alinesno-infra-plat-security"
+                        },
+                        {
+                            "id": 3,
+                            "name": "智能运营大脑服务",
+                            "desc": "用于智能运营大脑服务",
+                            "code": "alinesno-infra-plat-brain"
+                        },
+                        {
+                            "id": 5,
+                            "name": "移动终端服务",
+                            "desc": "用于移动终端服务",
+                            "code": "alinesno-infra-plat-app"
+                        }
+                    ]
+                }
+            ]
+        }
+    },
+    computed: {
+    },
+    methods: {
+        clickCoding(tag) {
+            ga('send', 'click', 'e.coding', 'Action', tag)
+        }
+    }
+}
+
+</script>
+
 
 <style lang="stylus">
 .crs-page-product-container {
--- docs/.vuepress/components/SolutionPage.vue
+++ docs/.vuepress/components/SolutionPage.vue
@@ -7,14 +7,14 @@
         </div> -->
 
         <div class="sulution-page-panel">
-            <div class="sulution-page-items-panel" v-for="item in 7" :key="item">
-                <div class="sulution-page-box">应用现代化改造</div>
+            <div class="sulution-page-items-panel" v-for="item in solutionList" :key="item.id">
+                <div class="sulution-page-box">{{ item.model }}</div>
                 <div class="sulution-page-box-desc">评估、规划、实施和衡量软件实践与功能，从而对贵组织的业务应用组合进行现代化改造和简化。</div>
                 <div class="item-list-box">
                     <ul>
-                        <li class="sulution-page-item" v-for="i in 8" :key="i">
-                            <div class="item-title">FinOps 和 GKE 优化</div>
-                            <div class="item-desc">在 GKE 上运行可靠、高性能且经济实惠的应用的最佳实践。</div>
+                        <li class="sulution-page-item" v-for="i in item.services" :key="i.id">
+                            <div class="item-title">{{ i.name }}</div>
+                            <div class="item-desc">{{ i.desc }}</div>
                         </li>
                     </ul>
                 </div>
@@ -23,6 +23,111 @@
 
     </div>
 </template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            solutionList: [
+                    {
+                        "id": 1,
+                        "model": "业务中台",
+                        "services": [
+                        {
+                            "id": 1,
+                            "name": "传统企业转变成业务中台解决方案",
+                            "desc": "该解决方案旨在帮助传统企业实现业务中台转型。",
+                            "code": "业务中台"
+                        },
+                        {
+                            "id": 2,
+                            "name": "企业业务中台建设解决方案",
+                            "desc": "该解决方案旨在帮助企业建设业务中台，实现业务的集中管理和协同发展。",
+                            "code": "业务中台"
+                        },
+                        {
+                            "id": 3,
+                            "name": "新旧结合业务微服务解决方案",
+                            "desc": "该解决方案结合了传统业务和微服务架构，帮助企业实现业务的快速创新和灵活扩展。",
+                            "code": "业务中台"
+                        },
+                        {
+                            "id": 4,
+                            "name": "企业数字化解决方案",
+                            "desc": "该解决方案旨在帮助企业实现数字化转型，提升业务流程的效率和用户体验。",
+                            "code": "业务中台"
+                        }
+                        ]
+                    },
+                    {
+                        "id": 2,
+                        "model": "数据治理",
+                        "services": [
+                        {
+                            "id": 1,
+                            "name": "数据中台建设解决方案",
+                            "desc": "该解决方案旨在帮助企业建设数据中台，实现数据的集中管理、质量控制和共享应用。",
+                            "code": "数据治理"
+                        },
+                        {
+                            "id": 2,
+                            "name": "数据报表分析解决方案",
+                            "desc": "该解决方案旨在帮助企业实现数据报表的自动化生成和分析，提供决策支持和业务洞察。",
+                            "code": "数据治理"
+                        },
+                        {
+                            "id": 3,
+                            "name": "用户画像解决方案",
+                            "desc": "该解决方案旨在帮助企业构建用户画像，深入了解用户需求和行为，进行个性化推荐和精准营销。",
+                            "code": "数据治理"
+                        },
+                        {
+                            "id": 4,
+                            "name": "运维应用画像解决方案",
+                            "desc": "该解决方案旨在帮助企业实现运维应用的画像管理，监控应用性能和故障，提高运维效率和系统稳定性。",
+                            "code": "数据治理"
+                        }
+                        ]
+                    },
+                    {
+                        "id": 4,
+                        "model": "运维方案",
+                        "services": [
+                        {
+                            "id": 1,
+                            "name": "自动化运维解决方案",
+                            "desc": "该解决方案旨在帮助企业实现运维流程的自动化，减少人工干预，提高运维效率和可靠性。",
+                            "code": "运维方案"
+                        },
+                        {
+                            "id": 2,
+                            "name": "分布式日志分析解决方案",
+                            "desc": "该解决方案旨在帮助企业对分布式系统的日志进行集中管理和分析，实现故障排查和性能优化。",
+                            "code": "运维方案"
+                        },
+                        {
+                            "id": 3,
+                            "name": "AI智能运维解决方案",
+                            "desc": "该解决方案旨在应用人工智能技术，实现智能化的运维管理，提升系统的稳定性和可靠性。",
+                            "code": "运维方案"
+                        }
+                        ]
+                    }
+                    ]
+
+        }
+    },
+    computed: {
+    },
+    methods: {
+        clickCoding(tag) {
+            ga('send', 'click', 'e.coding', 'Action', tag)
+        }
+    }
+}
+
+</script>
 
 <style lang="stylus">
 .sulution-page-product-container {
