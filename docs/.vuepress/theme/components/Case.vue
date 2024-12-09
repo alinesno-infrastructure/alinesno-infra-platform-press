2ea41dd4128dff892899d999b44eb6b258b844ab
梁明树(Nina) <ninaAip@gmail.com>
2024-12-10 00:32:55 +0800

    update: 更新案例图片并优化案例描述中的团队协作概念

--- docs/.vuepress/theme/components/Case.vue
+++ docs/.vuepress/theme/components/Case.vue
@@ -31,19 +31,19 @@ export default {
       caseList:
         [
           {
-            "img": "/assets/case/case_01.webp",
+            "img": "/assets/case/case_04.webp",
             "title": "多智能体协作网络",
             "sub": "通过智能体间的自动协作，优化业务流程，解放团队人力，使员工能够专注于更高价值的任务。"
           },
           {
-            "img": "/assets/case/case_03.webp",
+            "img": "/assets/case/case_05.webp",
             "title": "智能体驱动的工作自动化",
             "sub": "实现日常任务的自动化处理，减少人工干预，提高工作效率和准确性，支持团队专注于战略性和创造性工作。"
           },
           {
-            "img": "/assets/case/case_02.webp",
+            "img": "/assets/case/case_06.webp",
             "title": "多团队智能协作平台",
-            "sub": "提供一个可视化友好的管理平台，适配多部门和外包体系，促进多方合作，确保无缝沟通与协作。"
+            "sub": "提供一个可视化友好的管理平台，适配多部门和智能体团队，促进多方赋能，确保无缝沟通与协作。"
           }
         ]
     }
