4542bed35b4516c6ee6e4544e6a9a0e3bfc85d5e
李伟(Michael) <michaelAip@gmail.com>
2025-03-12 00:30:43 +0800

    feat:更新 Banner.vue 组件内容，优化展示效果和信息结构

--- docs/.vuepress/theme/components/Banner.vue
+++ docs/.vuepress/theme/components/Banner.vue
@@ -2,12 +2,14 @@
   <div class="banner-container">
     <div class="panel-left">
       <div class="notice-link-info">
-        <span class="link-text">新一代智能体平台服务在研发中</span>
-        <span class="link-icon">进度</span>
+        <span class="link-text">新一代智能体平台服务</span>
       </div>
       <div class="hero-header">
         <div class="hero-title">Multi-Agent平台 赋能团队自动化优化流程提升团队能力</div>
-        <div class="hero-sub-title">利用强大的多智能体优化行业，基于大型语言模型（LLM）构建并部署自动化工作流</div>
+        <div class="hero-sub-title">
+            <img src="http://staticok.oss-cn-hangzhou.aliyuncs.com/avatar-share/thumbnail-6046836d-7766-4bfd-a93d-85fd52d2b0e4.webp" style="width: 60px;border-radius: 50%;">
+            利用强大的多智能体优化传统业务流程，处理复杂任务，将不同的智能分配处理不同的任务，形成协作性。
+        </div>
       </div>
       <div class="hero-actions">
         <div class="action-link" style="background:#d33232">
@@ -16,15 +18,15 @@
           </a>
         </div> 
         <div class="action-saler">
-          <a href="https://www.bilibili.com/video/BV1PwCuY1Eu5" style="color: #fff;text-decoration: none;" target="_blank">
-            <i class="fa-solid fa-user-astronaut" style="font-size:25px;"></i> 演示视频合集
+          <a href="http://alinesno-agent.linesno.com/book/" style="color: #fff;text-decoration: none;" target="_blank">
+            <i class="fa-solid fa-book-journal-whills" style="font-size:25px;"></i> 多Agent协作与自动化电子书
           </a>
         </div> 
       </div>
     </div>
     <div class="panel-right">
       <div class="banner-img">
-        <img src="/assets/portal/book-cover.png" alt="" />
+        <img src="/assets/agent/agents.jpg" alt="" />
         <div class="img-text">
             <a href="http://alinesno-agent.linesno.com/book/" style="color: #fff;font-size: 15px;" target="_blank">
                超级个体:多Agent协作与自动化产品实践
